15abcb8954246d5edb563e72c9ea5567a5d43c2e
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-12-08 10:41:08 -0300

    .

--- basic-express-server/routes/index.js
+++ basic-express-server/routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/test', (req, res) => {
+  const person = {
+    name: "Esteban",
+    surname: "Toloza",
+    age: 27
+  }
+  res.json(person)
+});
+
+router.post('/test', (req, res) => {
+  const {body} = req;
+  
+  res.json(body)
+})
+
 module.exports = router;
