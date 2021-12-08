98ed3f273d8bbe91f2c8109288d984193a4d7726
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-12-08 12:22:49 -0300

    rutas dinamicas

--- basic-express-server/routes/index.js
+++ basic-express-server/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const users = [];
+let counter = 1;
 
 router.post('/user', (req, res) => {
   const {body} = req;
@@ -9,7 +10,15 @@ router.post('/user', (req, res) => {
   const {name} = body;
 
   if (body && name) {
-    users.push(name);
+
+    const data = {
+      id: counter,
+      name: name,
+    };
+
+    counter = counter + 1;
+    users.push(data);
+
     return res.sendStatus(200);
   } else {
     return res.sendStatus(400);
@@ -18,6 +27,21 @@ router.post('/user', (req, res) => {
 
 router.get('/user', (req, res) => {
   return res.json(users)
+})
+
+router.get('/user/:id', (req, res) => {
+  const { params } = req;
+
+  const user = users.find((user) => {
+    return user.id == params.id
+  })
+
+  if ( user ) {
+    return res.json(user)
+  } else {
+    return res.sendStatus(404)
+  };
+  
 })
 
 module.exports = router;
